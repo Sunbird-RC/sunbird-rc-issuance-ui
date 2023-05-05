296e50a0cef08508bbd08aa52dc111e25cadfa4e
Pratiksha Khandagale <pratiksha_k@techjoomla.com>
2023-05-05 15:55:09 +0530

    Task #210- Verify Flow for verifying VC

--- src/app/issure/verify/verify.component.ts
+++ src/app/issure/verify/verify.component.ts
@@ -6,8 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./verify.component.scss']
 })
 export class VerifyComponent implements OnInit {
+  itemData: any;
   constructor() {}
   ngOnInit(): void {
+    this.itemData =
+    {
+      "scanner_type": "ZBAR_QRCODE",
+      "scanNote": "To verify certificate, simply scan the QR code thats on the document.",
+      "verify_another_Certificate": 'Verify another Certificate',
+      "cetificate_not_valid": 'This Certificate is not valid',
+      "scan_qrcode_again": "Please scan QR code again"
+    }
   }
 
 }
